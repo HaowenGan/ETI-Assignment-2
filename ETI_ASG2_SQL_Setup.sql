371335a491ce9202bb3f56a2f05d127918c07a91
Joeygjh <Joeykkhgjh@gmail.com>
2024-02-08 18:39:15 +0800

    Added new table to SQL setup

--- ETI_ASG2_SQL_Setup.sql
+++ ETI_ASG2_SQL_Setup.sql
@@ -43,3 +43,11 @@ CREATE TABLE sections (
     content TEXT,
     FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS user_courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    course_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id)
+);
